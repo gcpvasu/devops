dcc3f382a987dcdff9f2fe580c4b8e2c90e728a9
vasuveera <vasu01veera@gmail.com>
2021-05-31 20:53:59 +0530

    added chef

---
+++ Chef/cookbooks/lampserver/recipes/default.rb
@@ -0,0 +1,47 @@
+#
+# Cookbook:: lampserver
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+
+# sudo apt update
+
+apt_update 'update packages' do
+    ignore_failure true
+    action :update
+end
+
+# sudo apt install apache2 -y
+
+package 'apache2' do
+    action :install
+end
+
+# sudo apt install php libapache2-mod-php php-mysql php-cli -y
+
+package 'php' do
+    action :install
+end
+
+package 'libapache2-mod-php' do
+    action :install
+end
+
+package 'php-mysql' do
+    action :install
+end
+
+package 'php-cli' do
+    action :install
+end
+
+# echo "<?php phpinfo(); ?>" | sudo tee /var/www/html/info.php
+
+file '/var/www/html/info.php' do
+    content '<?php phpinfo(); ?>'
+    action :create
+end
+
+
+
+
