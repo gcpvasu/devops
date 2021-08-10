758c65a9e0c0bf51244ce7b68fe541856dd3febe
practice <practice@gmail.com>
2021-08-10 14:38:33 +0530

    added security group and created instance

--- Terraform/ntieraws/main.tf
+++ Terraform/ntieraws/main.tf
@@ -46,3 +46,54 @@ resource "aws_route_table_association" "public_rt_association" {
     subnet_id = "${aws_subnet.subnets[var.web_subnet_indexes[count.index]].id}"
     route_table_id = "${aws_route_table.public_rt.id}"
 }
+
+
+resource "aws_security_group" "websg" {
+    name = "openhttp"
+    description = "openhttp and ssh"
+    vpc_id = "${aws_vpc.testvpc.id}"
+    tags = {
+      "Name" = "websg"
+    }
+}
+
+resource "aws_security_group_rule" "websgopenhttp" {
+    type = "ingress"
+    from_port = "80"    
+    to_port = "80"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = "${aws_security_group.websg.id}"
+}
+
+resource "aws_security_group_rule" "websgopenssh" {
+    type = "ingress"
+    from_port = "22"
+    to_port = "22"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = "${aws_security_group.websg.id}"
+}
+resource "aws_key_pair" "ntier" {
+    key_name = "ntier"     
+    public_key = "${file("~/Downloads/ntier.pem")}"           
+}
+resource "aws_instance" "webserver1" {
+    ami = "ami-09e67e426f25ce0d7"
+    instance_type = "t2.micro"
+    subnet_id = "${aws_subnet.subnets[0].id}"
+    associate_public_ip_address = true
+    key_name = "ntier"
+    security_groups = ["${aws_security_group.websg.id}"]
+    tags = {
+      "Name" = "webserver1"
+    }   
+}
+
+
+
+
+
+  
+
+  
